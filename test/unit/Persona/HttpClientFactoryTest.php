6d6a19995c56d9b97555a67867d0442658f22ee6
Mark Wallsgrove <markwallsgrove@users.noreply.github.com>
2018-11-29 12:23:50 +0000

    test http client factory

---
+++ test/unit/Persona/HttpClientFactoryTest.php
@@ -0,0 +1,130 @@
+<?php
+
+$appRoot = dirname(dirname(dirname(__DIR__)));
+require_once $appRoot . '/test/unit/TestBase.php';
+
+use \Talis\Persona\Client\Login;
+use \Talis\Persona\Client\HttpClientFactory;
+use \Doctrine\Common\Cache\ArrayCache;
+use \Guzzle\Http\Message\Response;
+use \Guzzle\Plugin\Mock\MockPlugin;
+use \Guzzle\Http\Exception\CurlException;
+
+class HttpClientFactoryTest extends TestBase
+{
+    public function testDefaultCaching()
+    {
+        $cacheBackend = $this->getMock(
+            '\Doctrine\Common\Cache\ArrayCache',
+            ['doSave']
+        );
+
+        $lifetimes = [];
+        $cacheBackend->expects($this->exactly(2))
+            ->method('doSave')
+            ->will($this->returnCallback(function($id, $data, $lifetime) use (&$lifetimes) {
+                array_push($lifetimes, $lifetime);
+            }));
+
+        $factory = new HttpClientFactory(
+            'http://localhost',
+            $cacheBackend
+        );
+
+        $httpClient = $factory->create();
+
+        $plugin = new MockPlugin();
+        $plugin->addResponse(new Response(200, [], 'body'));
+        $httpClient->addSubscriber($plugin);
+
+        $request = $httpClient->createRequest('get', '/test/path');
+        $response = $request->send();
+
+        $this->assertEquals([3600, 0], $lifetimes);
+    }
+
+    public function testSettingKeyPrefix()
+    {
+        $cacheBackend = $this->getMock(
+            '\Doctrine\Common\Cache\ArrayCache',
+            ['doSave']
+        );
+
+        $cacheBackend->expects($this->exactly(2))
+            ->method('doSave')
+            ->will($this->returnCallback(function($id, $data, $lifetime) {
+                $this->assertTrue(strpos($id, 'prefix_') === 1);
+            }));
+
+        $factory = new HttpClientFactory(
+            'http://localhost',
+            $cacheBackend,
+            ['keyPrefix' => 'prefix_']
+        );
+
+        $httpClient = $factory->create();
+
+        $plugin = new MockPlugin();
+        $plugin->addResponse(new Response(200, [], 'body'));
+        $httpClient->addSubscriber($plugin);
+
+        $request = $httpClient->createRequest('get', '/test/path');
+        $response = $request->send();
+    }
+
+    public function testSettingTtl()
+    {
+        $cacheBackend = $this->getMock(
+            '\Doctrine\Common\Cache\ArrayCache',
+            ['doSave']
+        );
+
+        $lifetimes = [];
+        $cacheBackend->expects($this->exactly(2))
+            ->method('doSave')
+            ->will($this->returnCallback(function($id, $data, $lifetime) use (&$lifetimes) {
+                array_push($lifetimes, $lifetime);
+            }));
+
+        $factory = new HttpClientFactory(
+            'http://localhost',
+            $cacheBackend,
+            ['defaultTtl' => 300]
+        );
+
+        $httpClient = $factory->create();
+
+        $plugin = new MockPlugin();
+        $plugin->addResponse(new Response(200, [], 'body'));
+        $httpClient->addSubscriber($plugin);
+
+        $request = $httpClient->createRequest('get', '/test/path');
+        $response = $request->send();
+
+        $this->assertEquals([300, 0], $lifetimes);
+    }
+
+    public function testSecondRequestUsesCache()
+    {
+        $cacheBackend = new ArrayCache();
+
+        $factory = new HttpClientFactory(
+            'http://localhost',
+            $cacheBackend
+        );
+
+        $httpClient = $factory->create();
+        $plugin = new MockPlugin();
+        $plugin->addResponse(new Response(200, [], 'body'));
+        $httpClient->addSubscriber($plugin);
+
+        $request = $httpClient->createRequest('get', '/test/path');
+        $response = $request->send();
+
+        $httpClient = $factory->create();
+        $request = $httpClient->createRequest('get', '/test/path');
+        $response = $request->send();
+
+        $this->assertEquals('body', $response->getBody());
+    }
+}
